50cdc3759b48090c736513943cb669bb10843318
candy <hae081128@naver.com>
2021-04-03 00:29:17 +0900

    h.w

--- variable.js
+++ variable.js
@@ -2,35 +2,36 @@
 
 
 function calculate(command, a, b){
-let result;
+
+  let result;
  
-  if(command ==='+'|'add'){
-    result = a+b;
-  console.log('result: ${result}');}
+  if((command === '+'||'add')){
+   result = a+b;
+   console.log('result:',result);}
 
-  else if(command ==='-'|'subtract'){
+    else if((command ==='-'||'subtract')){
     result = a-b;
-    console.log('result: ${result}');}
+    console.log('result:' ,result);}
 
-    else if(command ==='*'|'multiply'){
+    else if((command ==='*'||'multiply')){
     result=a*b;
-    console.log('result: ${result}');}
+    console.log('result:',result);}
 
-    else if(command ==='/'|'divide'){
+    else if((command ==='/'||'divide')){
     result= a/b;
-      if(result===0){
+      if((result===0)){
         result='0';}
-  console.log('result: ${result}');}
+      console.log('result:',result);}
 
-    else if(command ==='%'|'remainder'){
+    else if((command ==='%'||'remainder')){
     result = a%b;
-    console.log('result: ${result}');}
+    console.log('result:',result);}
 
       else {
       console.log('result: Wrong command!');}
   }
 
-  calculate('add', 1, 3);
+calculate('add', 1, 3);
 calculate('-', 1, 3);
 calculate('multiply', 1, 3);
 calculate('/', 4, 2);
